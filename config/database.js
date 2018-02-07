5d64ccf16e0e83b39125f614957a229090e977d3
phangni <phanganitrader@gmail.com>
2018-02-07 22:01:42 +0000

    api update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports = {
-    uri: 'mongodb://localhost:27017/' + this.db,
+    // uri: 'mongodb://localhost:27017/mean-angular',
+    uri: 'mongodb://levani:levani@ds229468.mlab.com:29468/mean-angular',
     secret: crypto,
     db: 'mean-angular'
 }
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const blogs = require('./routes/blogs')(router);
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 8080;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -13,12 +19,21 @@ mongoose.connect(config.uri, (err) => {
     }
 });
 
-app.use(express.static(__dirname + '/client/dist/'));
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use('/authentication', authentication);
+app.use('/blogs', blogs);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
 });
